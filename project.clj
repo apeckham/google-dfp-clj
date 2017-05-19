2aeb658cf07fe6f6067d2066ee34e5e150bb39c4
apeckham <apeckham@gmail.com>
2017-05-18 22:45:22 -0700

    add deps and instructions to get credentials.json

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  [com.google.api-ads/dfp-axis "3.2.0"]]
   :main ^:skip-aot google-dfp-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
