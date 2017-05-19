714804e993d646477e0e3091f00c4172a9a11a07
apeckham <apeckham@gmail.com>
2017-05-18 22:37:40 -0700

    initial

---
+++ test/google_dfp_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns google-dfp-clj.core-test
+  (:require [clojure.test :refer :all]
+            [google-dfp-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
